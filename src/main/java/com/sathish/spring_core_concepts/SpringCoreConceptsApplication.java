f3fd222d5d442cd48c97122b68a6961f3f79d5df
sathishreddy <sathishreddy.sr123@gmail.com>
2025-01-30 18:03:35 +0530

    initial commit

---
+++ src/main/java/com/sathish/spring_core_concepts/SpringCoreConceptsApplication.java
@@ -0,0 +1,13 @@
+package com.sathish.spring_core_concepts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCoreConceptsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCoreConceptsApplication.class, args);
+	}
+
+}
